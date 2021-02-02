6ea98f021e0276b37a5cc9c6e9f1e69a82474fb0
JMWoodgate <45073537+JMWoodgate@users.noreply.github.com>
2021-02-02 15:58:49 +0000

    added toString and getLongestSide functions

---
+++ Triangle.java
@@ -0,0 +1,35 @@
+class Triangle
+{
+    int longestSide, side1, side2, side3;
+
+
+    public Triangle(int a, int b, int c){
+        side1 = a;
+        side2 = b;
+        side3 = c;
+    }
+
+    int getLongestSide(){
+        if(side1 >= side2 && side1 >= side3){
+            longestSide = side1;
+        }
+        else if(side2 >= side1 && side2 >= side3){
+            longestSide = side2;
+        }
+        else if(side3 >= side1 && side3 >= side2){
+            longestSide = side3;
+        }
+        return longestSide;
+    }
+
+    String mytoString(Triangle newTriangle){
+        int longestSide = newTriangle.getLongestSide();
+        String newString = new String("The longest side of the triangle is " + longestSide);
+        return newString;
+    }
+
+    public static void main(String[] args){
+        Triangle myTriangle = new Triangle(2, 3, 5);
+        System.out.println(myTriangle.mytoString(myTriangle));
+    }
+}
