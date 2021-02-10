a728b1a10a50c93a506af47f3d701e59ec6ad1e8
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-02-10 14:30:03 +0000

    added triangle variants

--- Triangle.java
+++ Triangle.java
@@ -3,12 +3,65 @@ import java.lang.Math;
 class Triangle extends TwoDimensionalShape
 {
     int longestSide, side1, side2, side3;
-
+    private TriangleVariant variant;
 
     public Triangle(int a, int b, int c){
         side1 = a;
         side2 = b;
         side3 = c;
+        if(isIllegal(side1, side2, side3)){
+            variant = TriangleVariant.ILLEGAL;
+        }
+        else if(isRightAngled(side1, side2, side3)){
+            variant = TriangleVariant.RIGHT;
+        }
+        else if(isEquilateral(side1, side2, side3)){
+            variant = TriangleVariant.EQUILATERAL;
+        }
+        else if(isIsoceles(side1, side2, side3)){
+            variant = TriangleVariant.ISOSCELES;
+        }
+        else if(isScalene(side1, side2, side3)){
+            variant = TriangleVariant.SCALENE;
+        }
+        else{
+            variant = TriangleVariant.IMPOSSIBLE;
+        }
+    }
+
+    private boolean isRightAngled(int a, int b, int c){
+        if((a * a) + (b * b) == (c * c)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isIllegal(int a, int b, int c){
+        if(a + b < c || a + c < b || b + c > a){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isScalene(int a, int b, int c){
+        if(a != b || b != c || c != a){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isIsoceles(int a, int b, int c){
+        if(a == b || b == c || c == a){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isEquilateral(int a, int b, int c){
+        if(a == b && b == c){
+            return true;
+        }
+        return false;
     }
 
     public int getLongestSide(){
