c8a542b205d12e28137c7b86adaf091d0072abe9
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-02-16 15:54:55 +0000

    set coordinates in controller

--- OXO/OXOController.java
+++ OXO/OXOController.java
@@ -16,13 +16,28 @@ class OXOController
     {
         OXOPlayer currentPlayer = gameModel.getCurrentPlayer();
         System.out.println("command: " + command);
-        char array[] = command.toCharArray();
-        System.out.println("array: " + array);
-        int x = (int)array[1];
+        int length = command.length();
+        //char array[] = new char[length];
+        //System.out.println("array: " + array);
+        int x = (int)command.charAt(1);
+        x = (Character.getNumericValue(x)) - 1;
         System.out.println("x: "+ x);
-        int y = (int)array[0];
-        System.out.println("y: "+ y);
-        gameModel.setCellOwner(x, y, currentPlayer);
+        char y1 = command.charAt(0);
+        int y2 = (letterToNum(y1) - 1);
+        System.out.println("y: "+ y2);
+        gameModel.setCellOwner(y2, x, currentPlayer);
+    }
+
+    private int letterToNum(char letter)
+    {
+        char compare = 'a';
+        for(int i = 1; i < 27; i++){
+            if(letter == compare){
+                return i;
+            }
+            compare++;
+        }
+        return 0;
     }
 
 }
