1fc24bd96b611b363f163fce8e2bfd3ca8feb20b
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-02-16 16:47:13 +0000

    added checkGameWon function

--- OXO/OXOController.java
+++ OXO/OXOController.java
@@ -9,23 +9,83 @@ class OXOController
         OXOPlayer firstPlayer = new OXOPlayer('X');
         gameModel = model;
         gameModel.setCurrentPlayer(firstPlayer);
+        initWinThreshold(gameModel);
     }
-
 
     public void handleIncomingCommand(String command) throws OXOMoveException
     {
         OXOPlayer currentPlayer = gameModel.getCurrentPlayer();
-        System.out.println("command: " + command);
-        int length = command.length();
-        //char array[] = new char[length];
-        //System.out.println("array: " + array);
         int x = (int)command.charAt(1);
         x = (Character.getNumericValue(x)) - 1;
-        System.out.println("x: "+ x);
         char y1 = command.charAt(0);
         int y2 = (letterToNum(y1) - 1);
-        System.out.println("y: "+ y2);
         gameModel.setCellOwner(y2, x, currentPlayer);
+
+        if(gameModel.getWinner()==null){
+            changeCurrentPlayer(currentPlayer);
+        }
+        else{
+            return;
+        }
+    }
+
+    private boolean checkGameWon(OXOModel model)
+    {
+        int rowNum = model.getNumberOfRows();
+        int colNum = model.getNumberOfColumns();
+
+        for(int j = 0; j < rowNum; j++){
+            if(!checkRowWon(model, j)){
+                for(int i = 0; i < colNum; i++){
+                    if(checkColWon(model, i)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkColWon(OXOModel model, int colNum)
+    {
+        for(int i = 0; i < colNum - 1; i++){
+            if (gameModel.getCellOwner(i, colNum) != gameModel.getCellOwner(i + 1, colNum)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkRowWon(OXOModel model, int rowNum)
+    {
+        for(int i = 0; i < rowNum - 1; i++){
+            if (gameModel.getCellOwner(rowNum, i) != gameModel.getCellOwner(rowNum, i + 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void initWinThreshold(OXOModel model)
+    {
+        if(model.getNumberOfRows() < model.getNumberOfColumns()){
+            model.setWinThreshold(model.getNumberOfRows());
+        }
+        else{
+            model.setWinThreshold(model.getNumberOfColumns());
+        }
+    }
+
+    private void changeCurrentPlayer(OXOPlayer currentPlayer)
+    {
+        if('X' == currentPlayer.getPlayingLetter()){
+            OXOPlayer nextPlayer = new OXOPlayer('O');
+            gameModel.setCurrentPlayer(nextPlayer);
+        }
+        else{
+            OXOPlayer nextPlayer = new OXOPlayer('X');
+            gameModel.setCurrentPlayer(nextPlayer);
+        }
     }
 
     private int letterToNum(char letter)
