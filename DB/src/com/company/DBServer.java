ea21be3885c817512f97793b935effad59de0d93
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-03-15 10:48:26 +0000

    finished task 5

--- DB/src/com/company/DBServer.java
+++ DB/src/com/company/DBServer.java
@@ -38,28 +38,28 @@ class DBServer
         String folderName = File.separator + "Users" + File.separator + "jessw" + File.separator
                 + "Documents" + File.separator + "Java" + File.separator + "COMSM0086" +
                 File.separator + "Testfiles" + File.separator;
-        String newFolderName = File.separator + "Users" + File.separator + "jessw" + File.separator
-                + "Documents" + File.separator + "Java" + File.separator + "COMSM0086" +
-                File.separator + "DB" + File.separator + "Testfiles" + File.separator;
-
-        String incomingCommand = socketReader.readLine();
-        System.out.println("Received message: " + incomingCommand);
-        socketWriter.write("[OK] Thanks for your message: " + incomingCommand);
-        socketWriter.write("\n" + ((char)4) + "\n");
-        socketWriter.flush();
+        //String newFolderName = File.separator + "Users" + File.separator + "jessw" + File.separator
+                //+ "Documents" + File.separator + "Java" + File.separator + "COMSM0086" +
+                //File.separator + "DB" + File.separator + "Testfiles" + File.separator;
 
         try {
             FileIO fileIO = new FileIO(folderName);
             Database database = fileIO.readFolder(folderName);
             socketWriter.write(database.getDatabase());
-            ArrayList<String> listOfTableNames = database.getTableNames();
-            ArrayList<Table> tables = database.getTables();
-            for(int i = 0; i < database.getNumberOfTables(); i++){
-                fileIO.writeFile(newFolderName, listOfTableNames.get(i), tables.get(i));
-            }
+            socketWriter.flush();
+            //ArrayList<String> listOfTableNames = database.getTableNames();
+            //ArrayList<Table> tables = database.getTables();
+            //for(int i = 0; i < database.getNumberOfTables(); i++){
+            //    fileIO.writeFile(newFolderName, listOfTableNames.get(i), tables.get(i));
+            //}
         } catch(DBException | IOException e){
             e.printStackTrace();
         }
+
+        String incomingCommand = socketReader.readLine();
+        System.out.println("Received message: " + incomingCommand);
+        socketWriter.write("[OK] Thanks for your message: " + incomingCommand);
+        socketWriter.write("\n" + ((char)4) + "\n");
 
         try {
             Test testing = new Test();
