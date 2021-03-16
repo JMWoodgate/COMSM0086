399b4a02e856db8e171357b979be4df4d4212955
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-03-16 18:48:43 +0000

    written parseCreate

--- DB/src/com/company/Parsing/CreateCMD.java
+++ DB/src/com/company/Parsing/CreateCMD.java
@@ -1,16 +1,41 @@
 package com.company.Parsing;
 
+import com.company.DBExceptions.CommandException;
+import com.company.DBExceptions.StorageType;
+
 import java.util.ArrayList;
 
 public class CreateCMD {
 
     private final ArrayList<String> command;
-    private final int index;
+    private int index;
+    private StorageType type;
 
-    public CreateCMD(ArrayList<String> command, int index){
+    public CreateCMD(ArrayList<String> command, int index) throws CommandException {
         this.command = command;
         this.index = index;
+        if(!parseCreate()){
+            index++;
+            throw new CommandException(
+                    command.get(index), index, "table or database");
+        }
     }
 
+    private boolean parseCreate(){
+        String nextCommand = command.get(index+1);
+        switch(nextCommand){
+            case ("database"):
+                type = StorageType.DATABASE;
+                index++;
+                break;
+            case ("table"):
+                type = StorageType.TABLE;
+                index++;
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
 
 }
