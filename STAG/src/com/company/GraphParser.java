06bd61ad1ff36eeb40b50854640231107c40f96f
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-06-02 14:12:39 +0100

    starting to store variables after parsing entity file

--- STAG/src/com/company/GraphParser.java
+++ STAG/src/com/company/GraphParser.java
@@ -20,17 +20,23 @@ public class GraphParser {
 
             for(Graph g : subGraphs){
                 System.out.printf("id = %s\n",g.getId().getId());
+                String elementId = g.getId().getId();
                 ArrayList<Graph> subGraphs1 = g.getSubgraphs();
                 for (Graph g1 : subGraphs1){
                     ArrayList<Node> nodesLoc = g1.getNodes(false);
                     Node nLoc = nodesLoc.get(0);
                     System.out.printf("\tid = %s, name = %s\n",g1.getId().getId(), nLoc.getId().getId());
+                    String clusterId = g1.getId().getId();
+                    String clusterName = nLoc.getId().getId();
                     ArrayList<Graph> subGraphs2 = g1.getSubgraphs();
                     for (Graph g2 : subGraphs2) {
                         System.out.printf("\t\tid = %s\n", g2.getId().getId());
+                        String innerElementId = g2.getId().getId();
                         ArrayList<Node> nodesEnt = g2.getNodes(false);
                         for (Node nEnt : nodesEnt) {
                             System.out.printf("\t\t\tid = %s, description = %s\n", nEnt.getId().getId(), nEnt.getAttribute("description"));
+                            String elementTypeId = nEnt.getId().getId();
+                            String elementTypeDescription = nEnt.getAttribute("description");
                         }
                     }
                 }
