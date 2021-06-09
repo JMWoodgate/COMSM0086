4e995e46f321f7a6290f7454ded162b41b5d0d5a
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-06-09 12:09:41 +0100

    fixed multiple location storage

--- STAG/src/com/company/GraphParser.java
+++ STAG/src/com/company/GraphParser.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class GraphParser {
 
     private String entityFilename;
-    private ArrayList<Location> locations;
+    private final ArrayList<Location> locations;
 
     public GraphParser(String entityFilename){
         locations = new ArrayList<>();
@@ -21,35 +21,14 @@ public class GraphParser {
             parser.parse(reader);
             ArrayList<Graph> graphs = parser.getGraphs();
             ArrayList<Graph> subGraphs = graphs.get(0).getSubgraphs();
-
             for(Graph g : subGraphs){
                 System.out.printf("id1 = %s\n",g.getId().getId());
                 String elementId = g.getId().getId();
                 //if first element id is locations, need to create a new instance of locations and store values
                 //if it is paths, need to do something different
                 if(elementId.equals("locations")) {
-                    //create new location and store in array
-                    Location currentLocation = new Location();
-                    locations.add(currentLocation);
-                    ArrayList<Graph> subGraphs1 = g.getSubgraphs();
-                    for (Graph g1 : subGraphs1) {
-                        ArrayList<Node> nodesLoc = g1.getNodes(false);
-                        Node nLoc = nodesLoc.get(0);
-                        System.out.printf("\tid2 = %s, name = %s, description = %s\n", g1.getId().getId(), nLoc.getId().getId(), nLoc.getAttribute("description"));
-                        //store name and description of location
-                        storeNameDescription(currentLocation, nLoc);
-                        ArrayList<Graph> subGraphs2 = g1.getSubgraphs();
-                        for (Graph g2 : subGraphs2) {
-                            System.out.printf("\t\tid3 = %s\n", g2.getId().getId());
-                            String innerElementId = g2.getId().getId();
-                            ArrayList<Node> nodesEnt = g2.getNodes(false);
-                                for (Node nEnt : nodesEnt) {
-                                    System.out.printf("\t\t\tid4 = %s, description = %s\n", nEnt.getId().getId(), nEnt.getAttribute("description"));
-                                    storeDetails(currentLocation, innerElementId, nEnt.getAttribute("description"), nEnt.getId().getId());
-                                }
-                            }
-                        }
-                    }
+                    parseLocation(g);
+                }
                 ArrayList<Edge> edges = g.getEdges();
                 for (Edge e : edges){
                     System.out.printf("Path from %s to %s\n", e.getSource().getNode().getId().getId(), e.getTarget().getNode().getId().getId());
@@ -60,6 +39,7 @@ public class GraphParser {
         }
     }
 
+    //stores name and description of current location
     private void storeNameDescription(Location currentLocation, Node nLoc){
         String locationDescription = nLoc.getAttribute("description");
         String clusterName = nLoc.getId().getId();
@@ -67,6 +47,7 @@ public class GraphParser {
         currentLocation.setDescription(locationDescription);
     }
 
+    //stores description and id of artefact/furniture/characters in current location
     private void storeDetails(Location currentLocation, String dataType, String description, String id){
         switch(dataType){
             case "artefact":
@@ -80,6 +61,41 @@ public class GraphParser {
                 break;
             default:
                 //throw error - data type not recognised
+        }
+    }
+
+    private void parseLocation(Graph g){
+        ArrayList<Graph> subGraphs1 = g.getSubgraphs();
+        for (Graph g1 : subGraphs1) {
+            //create new location and store in array -- this is overwriting each time so need to change
+            Location currentLocation = new Location();
+            locations.add(currentLocation);
+            ArrayList<Node> nodesLoc = g1.getNodes(false);
+            Node nLoc = nodesLoc.get(0);
+            System.out.printf("\tid2 = %s, name = %s, description = %s\n", g1.getId().getId(), nLoc.getId().getId(), nLoc.getAttribute("description"));
+            //store name and description of location
+            storeNameDescription(currentLocation, nLoc);
+            ArrayList<Graph> subGraphs2 = g1.getSubgraphs();
+            //parse inner graphs
+            parseInnerGraphs(subGraphs2, currentLocation);
+        }
+    }
+
+    //loops through inner graphs to parse
+    private void parseInnerGraphs(ArrayList<Graph> subGraphs, Location currentLocation){
+        for (Graph g2 : subGraphs) {
+            System.out.printf("\t\tid3 = %s\n", g2.getId().getId());
+            String innerElementId = g2.getId().getId();
+            ArrayList<Node> nodesEnt = g2.getNodes(false);
+            parseNodes(nodesEnt, currentLocation, innerElementId);
+        }
+    }
+
+    //loop through nodes and parse
+    private void parseNodes(ArrayList<Node> nodesEnt, Location currentLocation, String id){
+        for (Node nEnt : nodesEnt) {
+            System.out.printf("\t\t\tid4 = %s, description = %s\n", nEnt.getId().getId(), nEnt.getAttribute("description"));
+            storeDetails(currentLocation, id, nEnt.getAttribute("description"), nEnt.getId().getId());
         }
     }
 }
